457f793bd7ec1f18e81cc794028f0d089dd7c39c
Rafee <156030423+rafeer192@users.noreply.github.com>
2024-06-24 12:44:34 -0700

    Handle digits and operator buttons

--- script.js
+++ script.js
@@ -28,3 +28,26 @@ function operate(num1, num2, symbol) {
 
 let operand1, operand2; 
 let operator; 
+let overallInput = "";    // entire sequence of chars (entire expression)
+const display = document.querySelector("div.display"); 
+const allBtns = document.querySelectorAll(".calculator button"); 
+allBtns.forEach( (btn) => btn.addEventListener("click", buttonHandler)); 
+
+function buttonHandler(event) {
+  if(event.target.classList.contains("digit")) {
+    if( overallInput.at(overallInput.length - 1)==='+' || overallInput.at(overallInput.length - 1)==='-' 
+        || overallInput.at(overallInput.length - 1)==='*' || overallInput.at(overallInput.length - 1)==='/') {
+      display.textContent = ""; 
+    }
+    display.textContent += event.target.textContent; 
+    overallInput += event.target.textContent; 
+  } 
+  else if(event.target.classList.contains("operator") &&!overallInput.includes('+') && !overallInput.includes('-')
+          && !overallInput.includes('*') && !overallInput.includes('/')) {
+    operator = event.target.textContent; 
+    operand1 = Number(display.textContent); 
+    overallInput += event.target.textContent; 
+  }
+}
+
+

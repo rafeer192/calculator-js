ba9e135bc5a68d9677240cfa32f35c74d7fef082
Rafee <156030423+rafeer192@users.noreply.github.com>
2024-06-25 08:11:01 -0700

    Create subhandler functions for each button type for clarity

--- script.js
+++ script.js
@@ -37,60 +37,79 @@ allBtns.forEach( (btn) => btn.addEventListener("click", buttonHandler));
 
 function buttonHandler(event) {
   const activeButton = document.querySelector("button.active"); 
-  if(activeButton) activeButton.classList.remove("active"); 
+  if(activeButton) {
+    activeButton.classList.remove("active"); 
+  } 
   event.target.classList.toggle("active");
 
   if(event.target.classList.contains("digit")) {
-    if( overallInput.at(overallInput.length - 1)==='+' || overallInput.at(overallInput.length - 1)==='-' 
-        || overallInput.at(overallInput.length - 1)==='*' || overallInput.at(overallInput.length - 1)==='/') {
-      display.textContent = "";   // reset
-    }
-    if(display.textContent.length <= MAX_DISPLAY_SIZE) {
-      display.textContent += event.target.textContent; 
-      overallInput += event.target.textContent; 
-    }
+    digitSubhandler(event);
   } 
   else if(event.target.classList.contains("operator")) {
-    operator = event.target.textContent; 
-    operand1 = Number(display.textContent); 
-    if(!overallInput.includes('+')&& !overallInput.includes('-')&& !overallInput.includes('*')&& !overallInput.includes('/')) {
-      overallInput += event.target.textContent; 
-    } else {
-      overallInput = overallInput.replace(/[+|*|/|-]/g, event.target.textContent); 
-    }
+    operatorSubhandler(event); 
   }
-  else if(event.target.textContent === '=') {
-    operand2 = Number(display.textContent);
-    if(operator === '/' && operand2 === 0) {
-      display.textContent = "nope, hit AC"; 
-    }
-    else if(overallInput.includes('+')||overallInput.includes('-')||overallInput.includes('*')||overallInput.includes('/')) {
-      let result = operate(operand1, operand2, operator); 
-      let resultLength = (result+'').length; 
-      if( resultLength > MAX_DISPLAY_SIZE && Number.isInteger(result) ) {
-        result = result.toExponential(4);
-      } else if( resultLength > MAX_DISPLAY_SIZE && !Number.isInteger(result) ) {
-        result = result.toFixed(resultLength - MAX_DISPLAY_SIZE); 
-      }
-      display.textContent = result; 
-      overallInput = String(result); 
-    } 
+  else if(event.target.classList.contains('=')) {
+    equalsSubhandler(event); 
   }
   else if(event.target.classList.contains("clear")) {
     display.textContent = ""; 
     overallInput = "";
   }
   else if(event.target.classList.contains("decimal")) {
-    if(!display.textContent.includes('.') && display.textContent.length <= MAX_DISPLAY_SIZE) {
-        //not already decimal in number, fits
-      display.textContent += '.'; 
-      overallInput += '.';
-    } else if(overallInput.endsWith('+')|| overallInput.endsWith('-')|| 
-              overallInput.endsWith('*') || overallInput.endsWith('/'))  {
-      display.textContent = '0.'; 
-      overallInput += '.';
-    }
+    decimalSubhandler(event); 
   }
 }
 
+function digitSubhandler(event) {
+  if( overallInput.endsWith('+') || overallInput.endsWith('-') || 
+      overallInput.endsWith('*') || overallInput.endsWith('/')) {
+    display.textContent = "";   // reset
+  }
+  if(display.textContent.length <= MAX_DISPLAY_SIZE) {
+    display.textContent += event.target.textContent; 
+    overallInput += event.target.textContent; 
+  }
+}
 
+function operatorSubhandler(event) {
+  operator = event.target.textContent; 
+  operand1 = Number(display.textContent); 
+  if(!overallInput.includes('+')&&!overallInput.includes('-')&&!overallInput.includes('*')&&!overallInput.includes('/')) { 
+    overallInput += event.target.textContent; 
+  } else {
+    overallInput = overallInput.replace(/[+|*|/|-]/g, event.target.textContent); 
+  }
+}
+
+function equalsSubhandler(event) {
+  operand2 = Number(display.textContent);
+  if(operator === '/' && operand2 === 0) {
+    display.textContent = "nope, hit AC"; 
+  }
+  else if(overallInput.includes('+')||overallInput.includes('-')||overallInput.includes('*')||overallInput.includes('/')) {
+    let result = operate(operand1, operand2, operator); 
+    let resultLength = (result+'').length; 
+    if(resultLength > MAX_DISPLAY_SIZE ) {
+      if(Number.isInteger(result)) {
+        result = result.toExponential(4);
+      } else {
+        result = result.toFixed(resultLength - MAX_DISPLAY_SIZE - 2); 
+      }
+    } 
+    display.textContent = result; 
+    overallInput = String(result); 
+  }
+}
+
+function decimalSubhandler(event) {
+  if(!display.textContent.includes('.') && display.textContent.length <= MAX_DISPLAY_SIZE) {
+    //not already decimal in number, fits
+    display.textContent += '.'; 
+    overallInput += '.';
+  } 
+  else if(overallInput.endsWith('+')|| overallInput.endsWith('-') || 
+          overallInput.endsWith('*') || overallInput.endsWith('/'))  {
+    display.textContent = '0.'; 
+    overallInput += '.';
+  }
+}
